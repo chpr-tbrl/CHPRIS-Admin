ef680b0be0f76ae2d2a19eb0d3bde463737318df
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2022-05-25 15:26:08 +0100

    update: add users page

--- src/pages/users.js
+++ src/pages/users.js
@@ -6,23 +6,98 @@ import {
   Modal,
   Column,
   FlexGrid,
+  DataTable,
   Pagination,
+  Table,
+  TableCell,
+  TableHead,
+  TableRow,
+  TableBody,
   TableToolbar,
+  TableHeader,
+  TableContainer,
+  TableBatchAction,
+  TableSelectRow,
   TableToolbarContent,
+  TableBatchActions,
   TableToolbarSearch,
 } from "@carbon/react";
 
 import {
-  Add,
   User,
   Renew,
   Person,
   Account,
   PillsAdd,
+  TrashCan,
+  Save,
 } from "@carbon/icons-react";
 
 const Records = () => {
   const [open, setOpen] = useState(false);
+  const [showActions, setShowActions] = useState(false);
+  const [selectedRow, setSelectedRow] = useState({});
+
+  const headers = [
+    {
+      key: "id",
+      header: "User ID",
+    },
+    {
+      key: "name",
+      header: "Name",
+    },
+    {
+      key: "role",
+      header: "Role",
+    },
+    {
+      key: "region",
+      header: "Region",
+    },
+    {
+      key: "site",
+      header: "Site",
+    },
+  ];
+
+  const rows = [
+    {
+      id: "1",
+      name: "Person one",
+      role: "Data-collector",
+      region: "North-West",
+      site: "Regional Hospital",
+    },
+    {
+      id: "2",
+      name: "Person two",
+      role: "Admin",
+      region: "North-West",
+      site: "Regional Hospital",
+    },
+    {
+      id: "3",
+      name: "Person three",
+      role: "Data-collector",
+      region: "North-West",
+      site: "Regional Hospital",
+    },
+    {
+      id: "4",
+      name: "Person four",
+      role: "Data-collector",
+      region: "North-West",
+      site: "Regional Hospital",
+    },
+  ];
+
+  function handleRowSelection(row) {
+    setShowActions(true);
+
+    let user = rows.find((user) => user.id === row.cells[0]?.value);
+    setSelectedRow(user);
+  }
 
   return (
     <FlexGrid fullWidth className="page">
@@ -31,75 +106,105 @@ const Records = () => {
         description="Manage and update all available users"
         renderIcon={<Account size={42} />}
       />
-      <Row>
-        <Column sm={0} md={8}>
-          <Spacer h={5} />
-          <TableToolbar>
-            <TableToolbarContent>
-              <TableToolbarSearch expanded />
-              <Button
-                kind="tertiary"
-                renderIcon={Renew}
-                iconDescription="refresh"
-                onClick={() => alert("button click")}
-              >
-                Refresh
-              </Button>
-              <Button
-                to="new"
-                renderIcon={Add}
-                iconDescription="New user"
-                onClick={() => alert("button click")}
-              >
-                New user
-              </Button>
-            </TableToolbarContent>
-          </TableToolbar>
-        </Column>
-        <Column sm={4} md={0}>
-          <Spacer h={5} />
-          <TableToolbar>
-            <TableToolbarContent>
-              <TableToolbarSearch expanded />
-              <Button
-                hasIconOnly
-                kind="tertiary"
-                renderIcon={Renew}
-                iconDescription="refresh"
-                onClick={() => alert("button clicked")}
-              />
-              <Button
-                hasIconOnly
-                renderIcon={Add}
-                iconDescription="new user"
-                onClick={() => alert("data")}
-              />
-            </TableToolbarContent>
-          </TableToolbar>
-        </Column>
-      </Row>
       <Spacer h={7} />
-      <Row>
-        <Column sm={4} md={4} lg={4} className="record--card__container">
-          record
-        </Column>
 
-        <Column>
-          <Spacer h={5} />
-          <h4
-            style={{
-              textAlign: "center",
-            }}
-          >
-            No available records
-          </h4>
-          <Spacer h={5} />
-        </Column>
-      </Row>
+      <DataTable rows={rows} headers={headers} radio>
+        {({
+          rows,
+          headers,
+          getHeaderProps,
+          getRowProps,
+          getToolbarProps,
+          getBatchActionProps,
+          onInputChange,
+          selectedRows,
+          getTableProps,
+          getSelectionProps,
+          getTableContainerProps,
+        }) => {
+          const batchActionProps = getBatchActionProps();
+
+          return (
+            <TableContainer
+              title="DataTable"
+              description="With batch actions"
+              {...getTableContainerProps()}
+            >
+              <TableToolbar {...getToolbarProps()}>
+                <TableBatchActions
+                  shouldShowBatchActions={showActions}
+                  onCancel={() => setShowActions(false)}
+                  totalSelected={1}
+                >
+                  <TableBatchAction
+                    tabIndex={batchActionProps.shouldShowBatchActions ? 0 : -1}
+                    renderIcon={TrashCan}
+                    onClick={() => console.log(selectedRows)}
+                  >
+                    Delete
+                  </TableBatchAction>
+                  <TableBatchAction
+                    tabIndex={batchActionProps.shouldShowBatchActions ? 0 : -1}
+                    renderIcon={Save}
+                    onClick={() => console.log(selectedRows)}
+                  >
+                    Update
+                  </TableBatchAction>
+                </TableBatchActions>
+                <TableToolbarContent
+                  aria-hidden={batchActionProps.shouldShowBatchActions}
+                >
+                  <TableToolbarSearch
+                    persistent
+                    tabIndex={batchActionProps.shouldShowBatchActions ? -1 : 0}
+                    onChange={onInputChange}
+                  />
+                  <Button
+                    tabIndex={batchActionProps.shouldShowBatchActions ? -1 : 0}
+                    onClick={() => alert("Add new row")}
+                    renderIcon={Renew}
+                    iconDescription="refresh"
+                  >
+                    Refresh
+                  </Button>
+                </TableToolbarContent>
+              </TableToolbar>
+              <Table {...getTableProps()}>
+                <TableHead>
+                  <TableRow>
+                    <th scope="col" />
+                    {headers.map((header, i) => (
+                      <TableHeader key={i} {...getHeaderProps({ header })}>
+                        {header.header}
+                      </TableHeader>
+                    ))}
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {rows.map((row, i) => (
+                    <TableRow
+                      key={i}
+                      {...getRowProps({ row })}
+                      onClick={() => handleRowSelection(row)}
+                    >
+                      <TableSelectRow
+                        {...getSelectionProps({ row })}
+                        checked={selectedRow.id === row.id}
+                      />
+                      {row.cells.map((cell) => (
+                        <TableCell key={cell.id}>{cell.value}</TableCell>
+                      ))}
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          );
+        }}
+      </DataTable>
 
       <Spacer h={7} />
       <Pagination pageSizes={[10, 20, 30, 40, 50]} totalItems={200} />
-
       <Modal
         open={open}
         passiveModal
