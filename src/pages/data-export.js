cef484a8fab643a9b8762d865fe603d17b28f887
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2022-06-03 11:00:24 +0100

    update: add checks for exportable range

--- src/pages/data-export.js
+++ src/pages/data-export.js
@@ -6,6 +6,7 @@ import {
   Stack,
   Button,
   Column,
+  Loading,
   FlexGrid,
   Dropdown,
   FormGroup,
@@ -13,6 +14,7 @@ import {
   InlineLoading,
   DatePickerInput,
   DropdownSkeleton,
+  InlineNotification,
 } from "@carbon/react";
 
 import { WatsonHealthDownloadStudy } from "@carbon/icons-react";
@@ -20,8 +22,9 @@ import { DATA_EXPORT_SCHEMA } from "schemas";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import {
-  useGetRegionsQuery,
   useGetSitesQuery,
+  useGetRegionsQuery,
+  useGetProfileQuery,
   useDataExportMutation,
 } from "services";
 import toast from "react-hot-toast";
@@ -46,6 +49,9 @@ const DataExport = () => {
     }
   );
 
+  const { data: profile = {}, isLoading: loadingProfile } = useGetProfileQuery(
+    auth.uid
+  );
   const [dataExport, { isLoading: isExporting }] = useDataExportMutation();
 
   const {
@@ -87,6 +93,14 @@ const DataExport = () => {
     });
   }
 
+  function getExportRange() {
+    const { exportable_range } = profile;
+    if (exportable_range) {
+      return exportable_range > 4 ? exportable_range - 1 : exportable_range;
+    }
+    return null;
+  }
+
   async function handleDataExport(data) {
     const API_URL = process.env.REACT_APP_API_URL;
     const request = {
@@ -99,12 +113,13 @@ const DataExport = () => {
       const response = await dataExport(request).unwrap();
       setDownload(API_URL + response);
       btnRef.current.click();
-      toast.success("Data export ready, please download");
+      toast.success("Data export ready");
     } catch (error) {
       // we handle errors with middleware
     }
   }
 
+  if (loadingProfile) return <Loading />;
   return (
     <FlexGrid fullWidth className="page">
       <PageHeader
@@ -114,122 +129,132 @@ const DataExport = () => {
       />
       <Spacer h={7} />
 
-      <Form onSubmit={handleSubmit(handleDataExport)}>
-        <Spacer h={5} />
-        <Stack gap={7}>
-          <FormGroup legendText="Region">
-            {!loadingRegions ? (
-              <Dropdown
-                id="region"
-                titleText=""
-                label="Select region"
-                items={[
-                  {
-                    id: "all",
-                    name: "all",
-                  },
-                  ...regions,
-                ]}
-                itemToString={(item) => item.name}
-                invalid={errors.region_id ? true : false}
-                invalidText={errors.region_id?.message}
-                onChange={(evt) => selectRegion(evt.selectedItem.id)}
-              />
-            ) : (
-              <DropdownSkeleton />
-            )}
-          </FormGroup>
-
-          <FormGroup legendText="Site">
-            {!loadingSites ? (
-              <Dropdown
-                id="site"
-                titleText=""
-                label={
-                  sites.length && regionId !== "all"
-                    ? "Select site"
-                    : regionId === "all"
-                    ? "Exporting for all sites"
-                    : "No available sites"
-                }
-                items={[
-                  {
-                    id: "all",
-                    name: "all",
-                  },
-                  ...sites,
-                ]}
-                itemToString={(item) => item.name}
-                invalid={errors.site_id ? true : false}
-                invalidText={errors.site_id?.message}
-                onChange={(evt) => selectSite(evt.selectedItem.id)}
-              />
-            ) : (
-              <DropdownSkeleton />
-            )}
-          </FormGroup>
-
-          <Row>
-            <Column>
-              <DatePicker
-                datePickerType="single"
-                maxDate={new Date()}
-                minDate={sub(new Date(), { months: 5 })}
-                onChange={(evt) => setStartDate(evt)}
-              >
-                <DatePickerInput
-                  id="start_date"
-                  placeholder="mm/dd/yyyy"
-                  labelText="Start date"
-                  invalid={errors.start_date ? true : false}
-                  invalidText={errors.start_date?.message}
+      {!getExportRange() ? (
+        <InlineNotification
+          kind="error"
+          hideCloseButton
+          lowContrast
+          title="Invalid permission"
+          subtitle="Sorry, you do not have the right permission to export data, contact administrator"
+        />
+      ) : (
+        <Form onSubmit={handleSubmit(handleDataExport)}>
+          <Spacer h={5} />
+          <Stack gap={7}>
+            <FormGroup legendText="Region">
+              {!loadingRegions ? (
+                <Dropdown
+                  id="region"
+                  titleText=""
+                  label="Select region"
+                  items={[
+                    {
+                      id: "all",
+                      name: "all",
+                    },
+                    ...regions,
+                  ]}
+                  itemToString={(item) => item.name}
+                  invalid={errors.region_id ? true : false}
+                  invalidText={errors.region_id?.message}
+                  onChange={(evt) => selectRegion(evt.selectedItem.id)}
                 />
-              </DatePicker>
-              <Spacer h={7} />
-            </Column>
-            <Column>
-              <DatePicker
-                datePickerType="single"
-                maxDate={new Date()}
-                minDate={sub(new Date(), { months: 5 })}
-                onChange={(evt) => setEndDate(evt)}
-              >
-                <DatePickerInput
-                  id="end_date"
-                  placeholder="mm/dd/yyyy"
-                  labelText="End date"
-                  invalid={errors.end_date ? true : false}
-                  invalidText={errors.end_date?.message}
+              ) : (
+                <DropdownSkeleton />
+              )}
+            </FormGroup>
+
+            <FormGroup legendText="Site">
+              {!loadingSites ? (
+                <Dropdown
+                  id="site"
+                  titleText=""
+                  label={
+                    sites.length && regionId !== "all"
+                      ? "Select site"
+                      : regionId === "all"
+                      ? "Exporting for all sites"
+                      : "No available sites"
+                  }
+                  items={[
+                    {
+                      id: "all",
+                      name: "all",
+                    },
+                    ...sites,
+                  ]}
+                  itemToString={(item) => item.name}
+                  invalid={errors.site_id ? true : false}
+                  invalidText={errors.site_id?.message}
+                  onChange={(evt) => selectSite(evt.selectedItem.id)}
                 />
-              </DatePicker>
-              <Spacer h={7} />
-            </Column>
-          </Row>
+              ) : (
+                <DropdownSkeleton />
+              )}
+            </FormGroup>
 
-          <Dropdown
-            id="format"
-            titleText="Export format"
-            label="Select format"
-            items={["csv", "pdf"]}
-            itemToString={(item) => item}
-            invalid={errors.format ? true : false}
-            invalidText={errors.format?.message}
-            onChange={(evt) => selectFormat(evt.selectedItem)}
-          />
+            <Row>
+              <Column>
+                <DatePicker
+                  datePickerType="single"
+                  maxDate={new Date()}
+                  minDate={sub(new Date(), { months: getExportRange() })}
+                  onChange={(evt) => setStartDate(evt)}
+                >
+                  <DatePickerInput
+                    id="start_date"
+                    placeholder="mm/dd/yyyy"
+                    labelText="Start date"
+                    invalid={errors.start_date ? true : false}
+                    invalidText={errors.start_date?.message}
+                  />
+                </DatePicker>
+                <Spacer h={7} />
+              </Column>
+              <Column>
+                <DatePicker
+                  datePickerType="single"
+                  maxDate={new Date()}
+                  minDate={sub(new Date(), { months: getExportRange() })}
+                  onChange={(evt) => setEndDate(evt)}
+                >
+                  <DatePickerInput
+                    id="end_date"
+                    placeholder="mm/dd/yyyy"
+                    labelText="End date"
+                    invalid={errors.end_date ? true : false}
+                    invalidText={errors.end_date?.message}
+                  />
+                </DatePicker>
+                <Spacer h={7} />
+              </Column>
+            </Row>
 
-          {isExporting ? (
-            <InlineLoading
-              status="active"
-              iconDescription="Active loading indicator"
-              description="processing ..."
+            <Dropdown
+              id="format"
+              titleText="Export format"
+              label="Select format"
+              items={["csv", "pdf"]}
+              itemToString={(item) => item}
+              invalid={errors.format ? true : false}
+              invalidText={errors.format?.message}
+              onChange={(evt) => selectFormat(evt.selectedItem)}
             />
-          ) : (
-            <Button ref={btnRef} href={download} type="submit">
-              Query and download
-            </Button>
-          )}
-        </Stack>
-      </Form>
+
+            {isExporting ? (
+              <InlineLoading
+                status="active"
+                iconDescription="Active loading indicator"
+                description="processing ..."
+              />
+            ) : (
+              <Button ref={btnRef} href={download} type="submit">
+                Query and download
+              </Button>
+            )}
+          </Stack>
+        </Form>
+      )}
     </FlexGrid>
   );
 };
